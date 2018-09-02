c8b131e6d57b8f4300e812b46adaefc923ba5ca9
장지훈 <s5v7@outlook.kr>
2018-09-02 16:06:20 +0900

    로그인 하지 않았을 경우 로그인 페이지로 Redirect

--- CouncilEvent/Default.aspx.cs
+++ CouncilEvent/Default.aspx.cs
@@ -1,14 +1,13 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.UI;
-using System.Web.UI.WebControls;
 
 namespace CouncilEvent {
 	public partial class Default : System.Web.UI.Page {
 		protected void Page_Load(object sender, EventArgs e) {
-
+			// 세션이 비어 있을 경우
+			if (Session["UserID"] == null) {
+				// 로그인 페이지로 Redirect
+				Response.Redirect("/Login.aspx");
+			}
 		}
 	}
 }
