e9dbac197a99269e8a7daececffb9f6707358717
장지훈 <s5v7@outlook.kr>
2018-09-02 19:12:31 +0900

    점수를 부여하는 메서드 추가

--- CouncilEvent/managers/EventManager.cs
+++ CouncilEvent/managers/EventManager.cs
@@ -1,6 +1,37 @@
-﻿namespace CouncilEvent.managers {
+﻿using CouncilEvent.Models;
+using MySql.Data.MySqlClient;
+using System;
+
+namespace CouncilEvent.managers {
 	public class EventManager {
 		// Table Name
 		private const string EVENTS = "events";
+
+		/// <summary>
+		/// 점수주는 메서드
+		/// </summary>
+		/// <param name="event1">Event class in Models</param>  
+		/// <see cref="Event"/>
+		public static void AddScore(Event event1) {
+			// Connect to DB
+			using (var conn = new MySqlConnection(System.Configuration.ConfigurationManager.ConnectionStrings["COUNCILDB"].ConnectionString)) {
+				conn.Open();
+
+				// Command Text - Create
+				string commandText = "INSERT INTO " + EVENTS + "(UserID, Score, SingerNumber) VALUES (?, ?, ?);";
+				var cmd = new MySqlCommand(commandText, conn);
+				cmd.Parameters.Add("UserID", MySqlDbType.VarChar).Value = event1.UserID;
+				cmd.Parameters.Add("Score", MySqlDbType.Int32).Value = event1.Score;
+				cmd.Parameters.Add("SingerNumber", MySqlDbType.Int32).Value = event1.SingerNumber;
+
+				try {
+					cmd.ExecuteNonQuery();
+				} catch (Exception e) {
+				}
+
+				// Connection Close
+				conn.Close();
+			}
+		}
 	}
 }
