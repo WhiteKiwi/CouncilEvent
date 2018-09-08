27fc52c65f74c4f78095f79e1ea141b3700e9830
장지훈 <s5v7@outlook.kr>
2018-09-08 14:42:39 +0900

    평균 점수를 계산 하는 프로그램 추가

---
+++ Score/Program.cs
@@ -0,0 +1,32 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+
+namespace Score {
+	class Program {
+		static void Main(string[] args) {
+			// Connect to DB
+			using (var conn = new MySqlConnection("Server=45.76.222.237;Database=CNSACouncil;UID=dbcouncil;password=cnsa140301;CharSet=utf8;SslMode=none;")) {
+				conn.Open();
+
+				Console.Write("참가자 번호: ");
+				string singerNumber = Console.ReadLine();
+
+				string commandText = "SELECT Score FROM events WHERE SingerNumber='"+singerNumber+"';";
+				var cmd = new MySqlCommand(commandText, conn);
+
+				long score = 0;
+				int count = 0;
+				var rdr = cmd.ExecuteReader();
+				while (rdr.Read()) {
+					score += Convert.ToInt32(rdr["Score"]);
+					count++;
+				}
+
+				Console.WriteLine("평균점수: " + ((float)score / (count == 0 ? 1 : count)));
+
+				// Connection Close
+				conn.Close();
+			}
+		}
+	}
+}
